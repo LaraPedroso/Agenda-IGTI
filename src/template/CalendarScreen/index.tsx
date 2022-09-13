b8d569bc66402d0d1fae6d6a5c05ae850e421513
Lara <pedroso5@hotmail.com>
2022-09-13 16:48:56 -0300

    table does not work 😭😢

--- src/template/CalendarScreen/index.tsx
+++ src/template/CalendarScreen/index.tsx
@@ -1,24 +1,17 @@
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  // TableCell,
-  TableBody,
-  Paper
-} from '@mui/material'
+import // TableContainer,
+// Table,
+// TableHead,
+// TableRow,
+// TableCell,
+// TableBody,
+// Paper
+'@mui/material'
+import { Typography } from '@mui/material'
 
 export function CalendarScreen() {
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow></TableRow>
-          </TableHead>
-          <TableBody></TableBody>
-        </Table>
-      </TableContainer>
+      <Typography>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</Typography>
     </>
   )
 }
